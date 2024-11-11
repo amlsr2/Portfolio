dfe919997056c9c00475e12410cee68b60635d32
unknown <Ameliasari.umb@gmail.com>
2024-11-11 14:42:40 +0700

    new vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
@@ -16,19 +15,8 @@ import App from "./App";
 import "./index.css"; // jika ada file CSS global
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';  // jika ada file CSS global
-
-ReactDOM.createRoot(document.getElementById('root')).render(
->>>>>>> 90ab0809cfa1dd12a7825b4d8d90dd4f61575ad6
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
 
-=======
->>>>>>> 90ab0809cfa1dd12a7825b4d8d90dd4f61575ad6

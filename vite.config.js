6760c4f65fc1b2e7d42f83b52fa5b65b8c572ffb
unknown <Ameliasari.umb@gmail.com>
2024-11-12 15:38:35 +0700

    larry3

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "./", // Ubah output build ke dalam folder src atau folder lain yang Anda inginkan
-    emptyOutDir: false, // agar tidak menghapus folder src saat build
-  },
 });
